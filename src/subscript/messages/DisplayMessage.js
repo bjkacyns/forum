081cb26ccd1d42a6f142fe646d283d5b16e7e99e
bjkacyns <77307532+bjkacyns@users.noreply.github.com>
2021-04-11 15:09:57 -0400

    Add files via upload

---
+++ src/subscript/messages/DisplayMessage.js
@@ -0,0 +1,34 @@
+const DisplayMessage = ({info}) => {
+    const t = window.location.pathname;
+    if (t === "/") { //home - display all messages
+        return ( 
+            <div className="DisplayMessage">
+                <h2>My messages:</h2>
+                {info.sort((a,b) => b.id - a.id).map(MU => (
+                   <div id="previewMessage" key={MU.id}>
+                            <div className="messageContent"> "{MU.message}"</div>
+                            <div className="userInfo"> - {MU.user}</div>
+                            <div className="idContent"> (ID: {MU.id})</div>
+                   </div>
+               ))}
+            </div>
+         );
+    } else { //display messages only for that thread
+        const newt = t.substring(9, t.length);
+
+        return ( 
+            <div className="DisplayMessage">
+                <h2>My messages:</h2>
+                {info.filter(MU => MU.threadID === newt).sort((a,b) => b.id - a.id).map(MU => (
+                   <div id="previewMessage" key={MU.id}>
+                            <div className="messageContent"> "{MU.message}"</div>
+                            <div className="userInfo"> - {MU.user}</div>
+                            <div className="idContent"> (ID: {MU.id})</div>
+                   </div>
+               ))}
+            </div>
+         );
+    }  
+}
+
+export default DisplayMessage;
